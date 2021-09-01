8ad8768199b13eea42e6a3a1b23fdebcea87b44d
myusername <myemail>
2021-09-01 16:21:49 -0500

    lastphase

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ function App() {
                 featherCount={featherCount}
                 featherColors={featherColors}
             />
-            <Message size={size} />
+            <Message size={size} featherCount={featherCount} />
         </>
     );
 }
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,45 @@
-import { useEffect } from 'react';
 
-function Message({ size }) {
+import { useEffect, useState } from 'react';
+
+function Message({ size, featherCount }) {
+    const [message, setMessage] = useState('');
+    const [sizeClass, setSizeClass] = useState('');
+
     useEffect(() => {
-        console.log('Message', size);
-    },[size])
+        if (featherCount <= 0)
+            setMessage('Oh my! Your bird is naked!');
+        else if (featherCount >= 10) {
+            setMessage('Full turkey!');
+        } else {
+            setMessage('Coming along...');
+        }
+    }, [featherCount])
+
+    useEffect(() => {
+        console.log('PictureDisplay size', size);
+        let cname = ''
+        // if what you click on is the case, then the cname is going to be set to medium
+        switch (size) {
+            case 'm':
+                cname = 'medium';
+                break;
+            case 'l':
+                cname = 'large';
+                break;
+            case 'xl':
+                cname = 'xlarge';
+                break;
+            default:
+                cname = 'small';
+                break;
+        }
+        console.log(cname);
+        // does not want to mutate the state, setSizeClass = cname
+        setSizeClass(cname);
+    }, [size])
 
     return (
-        <div className="message medium">
+        <div className={`message ${sizeClass}`}>
             (Oh my! Your bird is naked!)
         </div>
     );
